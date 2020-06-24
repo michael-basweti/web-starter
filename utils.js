4cbc6691e476c6684d477f32259918c5af64b0e5
michael-basweti <baswetima@gmail.com>
2020-06-24 09:24:16 +0300

    detecting clicks

--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
 function showMessage(message){
     document.getElementById('message').textContent = message
 }
+
+const button = document.getElementById('see-review')
+
+button.addEventListener('click', function(){
+    console.log('clicked');
+    
+})
